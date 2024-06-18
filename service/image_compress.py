cf6502f2ea2200f62083ce4198694a7783cd59e8
Shaammiru <msyamil.hamami@gmail.com>
2024-06-19 00:05:11 +0700

    feat: add image and video compress

--- service/image_compress.py
+++ service/image_compress.py
@@ -3,33 +3,15 @@ from PIL import Image
 from io import BytesIO
 
 
-def compress_image_jpeg(image_buffer):
+def compress_image(image_buffer: BytesIO, algorithm: str):
     output_buffer = BytesIO()
     start_time = time.time()
 
     image = Image.open(image_buffer)
-    image.save(output_buffer, "JPEG", quality=80)
+    image.save(output_buffer, algorithm, quality=80)
 
     end_time = time.time()
     process_time = end_time - start_time
     output_buffer.seek(0)
 
     return output_buffer, process_time
-
-
-def compress_image_webp(image_buffer):
-    output_buffer = BytesIO()
-    start_time = time.time()
-
-    image = Image.open(image_buffer)
-    image.save(output_buffer, "WEBP", quality=80)
-
-    end_time = time.time()
-    process_time = end_time - start_time
-    output_buffer.seek(0)
-
-    return output_buffer, process_time
-
-
-buffer, total_time = compress_image_webp("test.png")
-print(f"{total_time:.2f} Second")
